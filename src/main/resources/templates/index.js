f0686036689e1f92b852e403d1e8f48170a14c9d
Sh1re-a <83682762+Sh1re-a@users.noreply.github.com>
2022-02-09 15:00:38 +0100

    .....

--- src/main/resources/templates/index.js
+++ src/main/resources/templates/index.js
@@ -1,3 +1,24 @@
-document.querySelector("#cBox2").addEventListener("click",function()) {
+
+
+function cateClick() {
+
     
+    document.getElementById("desText").innerHTML = "Pre";
+    document.getElementById("desText").style.color = "white";
+    var elements = document.getElementsByClassName('dessertBox'); // get all elements
+
+    var dates = document.querySelectorAll('[id^="dBox"]');
+    for (var i = 0; i < dates.length; i++){
+      elements[i].style.transition = "0.7s"
+      elements[i].style.color = "white";
+      
+    }
+  
+}
+
+function cateClick1(el){
+  var val = el.id;
+  var x = document.getElementById(el).innerText;
+  document.getElementById("desText").innerHTML = x;
+
 }
