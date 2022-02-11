d87b3b152d11964332c0e23348d656cb86e75288
Sh1re-a <83682762+Sh1re-a@users.noreply.github.com>
2022-02-11 23:05:09 +0100

    Progress FrontEnd

--- src/main/resources/templates/index.js
+++ src/main/resources/templates/index.js
@@ -18,7 +18,58 @@ function cateClick() {
 
 function cateClick1(el){
   var val = el.id;
-  var x = document.getElementById(el).innerText;
+  var x = document.getElementById(el).innerHTML;
   document.getElementById("desText").innerHTML = x;
 
+}
+
+var one = 0;
+var two = 0;
+var three = 0;
+var four = 0;
+var five = 0;
+var six = 0;
+var seven = 0;
+var eight = 0;
+var nine = 0;
+function itemCountFunction(idElement){
+  if(idElement.includes("1")){
+    document.getElementById("itemcount1").innerHTML = one++;
+  }
+  if(idElement.includes("2")){
+    
+    document.getElementById("itemcount2").innerHTML = two++;
+  }
+  if(idElement.includes("3")){
+    
+    document.getElementById("itemcount3").innerHTML = three++;
+  }
+  if(idElement.includes("4")){
+    
+    document.getElementById("itemcount4").innerHTML = four++;
+  }
+  if(idElement.includes("5")){
+    
+    document.getElementById("itemcount5").innerHTML = five++;
+  }
+  if(idElement.includes("6")){
+    
+    document.getElementById("itemcount6").innerHTML = six++;
+  }
+  if(idElement.includes("7")){
+   
+    document.getElementById("itemcount7").innerHTML = seven++;
+  }
+  if(idElement.includes("8")){
+    
+    document.getElementById("itemcount8").innerHTML = eight++;
+  }
+  if(idElement.includes("9")){
+
+    document.getElementById("itemcount9").innerHTML = nine++;
+  }
+
+
+  
+  
 }
