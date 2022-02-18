ed35e4324941ab5ad16dd0ed5d1654b8d37c8f58
Sh1re-a <83682762+Sh1re-a@users.noreply.github.com>
2022-02-18 19:50:53 +0100

    Layout using grid

--- src/main/java/se/shirwac/pos/system/controller/AdminController.java
+++ src/main/java/se/shirwac/pos/system/controller/AdminController.java
@@ -1,2 +1,42 @@
-package se.shirwac.pos.system.controller;public class AdminController {
+package se.shirwac.pos.system.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import se.shirwac.pos.system.models.Dish;
+import se.shirwac.pos.system.repo.DishRepo;
+
+import java.util.List;
+
+@RestController
+public class AdminController {
+
+    @Autowired
+    private DishRepo dishRepo;
+
+    @GetMapping(value = "/")
+    public String welcomePage(){
+        return "Welcome to my page";
+    }
+
+    @GetMapping(value = "/allDishes")
+    public List<Dish> getAllDishes(){
+        return dishRepo.findAll();
+    }
+
+    @PostMapping(value = "/save/dish")
+    public String saveDish(@RequestBody Dish dish){
+        dishRepo.save(dish);
+        return "Dish is saved";
+
+    }
+
+    @DeleteMapping(value = "/delete/dish")
+    public String deleteDish(@PathVariable long id,@RequestBody Dish dish){
+        Dish deleteDish = dishRepo.findById(id).get();
+        dishRepo.delete(deleteDish);
+        return "Dish" + deleteDish.getName() + " is deleted";
+
+    }
+
+
 }
