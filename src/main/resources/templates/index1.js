962ae3f40b4e788231cae800a7564a4b23a8598e
Sh1re-a <83682762+Sh1re-a@users.noreply.github.com>
2022-02-20 00:12:06 +0100

    Text change after button pressed

--- src/main/resources/templates/index1.js
+++ src/main/resources/templates/index1.js
@@ -2,17 +2,50 @@ function showDishes(idElement){
     var id = idElement.id
     var idElement = document.getElementById(id)
     var categoryText = idElement.innerHTML
-    var bodyElement = document.getElementById("content2Text1")
+   
 
     var stringCut = categoryText.split('<span')[0]
 
+    
+
+  
+  
+  /*  var createSpan1 = document.createElement("span")
+    createSpan1.setAttribute("class", "boxHeader1")
+    createSpan1.innerHTML = stringCut
+
+    var element = document.getElementById("BoxHeader")
+    element.appendChild(createSpan1)
+
+    var createSpan2 = document.createElement("span")
+    createSpan.setAttribute("class", "content2Text1")
+    createSpan2.innerHTML = "Popular A/z Price"
+
+    var element2 = document.getElementById("hej")
+    element2.appendChild(createSpan2)
+    
+    */
+
+    
+    
+   
+
+   
+
+   document.getElementById("BoxHeader").innerHTML = stringCut +"<div id ='hej'class='content2Text1'><span class='headtext1'>Popular</span><span class='headtext2'>A/z</span><span class='headtext3'>Price</span></div>"
+   // var colorElement = document.getElementById("true")
+  //  colorElement.style.color = "black"
+}
+
+function createSpan(){
     var createSpan = document.createElement('span')
     
+    createSpan.setAttribute("class", "content2Text1")
+    var ja = createSpan.innerHTML = "Popular A/z Price"
     
-    createSpan.setAttribute("class", "itemCount")
-    createSpan.innerHTML = "10 items"
 
-    document.getElementById("BoxHeader").innerHTML = stringCut 
+    createSpan.innerHTML = "Popular A/z Price"
 
+    return createSpan
 
 }
