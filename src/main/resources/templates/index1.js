902068d1fd6852670d1051ccba0920730cc07107
Sh1re-a <83682762+Sh1re-a@users.noreply.github.com>
2022-02-20 03:39:07 +0100

    Working on adding item to the cashier

--- src/main/resources/templates/index1.js
+++ src/main/resources/templates/index1.js
@@ -1,9 +1,11 @@
+
+
 function showDishes(idElement){
     var id = idElement.id
     var idElement = document.getElementById(id)
     var categoryText = idElement.innerHTML
     var stringCut = categoryText.split('<span')[0]
-    document.getElementById("BoxHeader").innerHTML = stringCut +"<div id ='hej'class='content2Text1'><span class='headtext1'>Popular</span><span class='headtext2'>A/z</span><span class='headtext3'>Price</span></div>"
+    document.getElementById("BoxHeader").innerHTML = stringCut +"<div id ='hej'class='content2Text1'><span class='headtext1'>Popular</span>&nbsp; &nbsp;&nbsp;<span class='headtext2'>A/z</span>&nbsp; &nbsp;&nbsp;<span class='headtext3'>Price</span></div>"
   
     
 
@@ -13,12 +15,6 @@ function showDishes(idElement){
     var totalDish = 9;
 
     createDishes(totalDish)
-
-
-
-   
-    
-
 
 }
 
@@ -37,4 +33,38 @@ function createDishes(totalDish){
     var divId = document.getElementById("2")
     divId.appendChild(createContent)
     }
+}
+
+
+function addToCashier(idBox){
+    //TODO: Skapa en loop som lägger till
+
+    var id = idBox.id
+    var idElementItem = document.getElementById(id).getElementsByClassName("item")[0]
+    var idElementPrice= document.getElementById(id).getElementsByClassName("price")[0]
+    
+    var item = [];
+    item.push(idElementItem)
+    
+    var price = idElementPrice.innerHTML
+
+    var getCategory = document.getElementById("true")
+    var categoryText = getCategory.innerHTML
+
+    
+    var createContent = document.createElement("div")
+
+    createContent.setAttribute("class", "content3text1")
+    createContent.innerHTML = categoryText + allItem(item) + price
+
+    var divId = document.getElementById("3")
+    divId.appendChild(createContent)
+
+}
+
+function allItem(array){
+    for(var i = 0; i < array.length; i++){
+       return array[i];
+    }
+
 }
