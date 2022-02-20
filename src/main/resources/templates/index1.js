f5e14c887b0146ab8d76726dce79e71eba189399
Sh1re-a <83682762+Sh1re-a@users.noreply.github.com>
2022-02-20 02:21:17 +0100

    Finish with showDishes, createDishes and removeDishes function

--- src/main/resources/templates/index1.js
+++ src/main/resources/templates/index1.js
@@ -2,50 +2,39 @@ function showDishes(idElement){
     var id = idElement.id
     var idElement = document.getElementById(id)
     var categoryText = idElement.innerHTML
-   
-
     var stringCut = categoryText.split('<span')[0]
-
-    
-
+    document.getElementById("BoxHeader").innerHTML = stringCut +"<div id ='hej'class='content2Text1'><span class='headtext1'>Popular</span><span class='headtext2'>A/z</span><span class='headtext3'>Price</span></div>"
   
-  
-  /*  var createSpan1 = document.createElement("span")
-    createSpan1.setAttribute("class", "boxHeader1")
-    createSpan1.innerHTML = stringCut
-
-    var element = document.getElementById("BoxHeader")
-    element.appendChild(createSpan1)
-
-    var createSpan2 = document.createElement("span")
-    createSpan.setAttribute("class", "content2Text1")
-    createSpan2.innerHTML = "Popular A/z Price"
-
-    var element2 = document.getElementById("hej")
-    element2.appendChild(createSpan2)
     
-    */
 
-    
-    
-   
+    var element = document.getElementsByClassName("contentBox2")
+    removeDishes(element)
+
+    var totalDish = 9;
+
+    createDishes(totalDish)
+
+
 
    
+    
 
-   document.getElementById("BoxHeader").innerHTML = stringCut +"<div id ='hej'class='content2Text1'><span class='headtext1'>Popular</span><span class='headtext2'>A/z</span><span class='headtext3'>Price</span></div>"
-   // var colorElement = document.getElementById("true")
-  //  colorElement.style.color = "black"
+
 }
 
-function createSpan(){
-    var createSpan = document.createElement('span')
-    
-    createSpan.setAttribute("class", "content2Text1")
-    var ja = createSpan.innerHTML = "Popular A/z Price"
-    
+function removeDishes(elements){
+    for(var i = elements.length - 1; 0 < elements.length; i--){
+        elements[i].parentNode.removeChild(elements[i]);
+    }
 
-    createSpan.innerHTML = "Popular A/z Price"
+}
 
-    return createSpan
-
+function createDishes(totalDish){
+    for(var i = 0; i < totalDish; i++){
+        var createContent = document.createElement("div")
+    createContent.setAttribute("class", "contentBox2")
+    createContent.innerHTML = "nya"
+    var divId = document.getElementById("2")
+    divId.appendChild(createContent)
+    }
 }
