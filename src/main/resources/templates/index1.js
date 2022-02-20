535e9a1a43671ab402fa5e1f2ae0113f4eb19a99
Sh1re-a <83682762+Sh1re-a@users.noreply.github.com>
2022-02-20 05:03:37 +0100

    Trying to create loop so I dont get duplicate

--- src/main/resources/templates/index1.js
+++ src/main/resources/templates/index1.js
@@ -6,7 +6,7 @@ function showDishes(idElement){
     var categoryText = idElement.innerHTML
     var stringCut = categoryText.split('<span')[0]
     document.getElementById("BoxHeader").innerHTML = stringCut +"<div id ='hej'class='content2Text1'><span class='headtext1'>Popular</span>&nbsp; &nbsp;&nbsp;<span class='headtext2'>A/z</span>&nbsp; &nbsp;&nbsp;<span class='headtext3'>Price</span></div>"
-  
+    var cutIdNumber = id.split('cbox')
     
 
     var element = document.getElementsByClassName("contentBox2")
@@ -15,6 +15,28 @@ function showDishes(idElement){
     var totalDish = 9;
 
     createDishes(totalDish)
+
+    var createContent = document.createElement("div")
+    createContent.setAttribute("class", "content3text1")
+    createContent.setAttribute("id", cutIdNumber )
+    var divId = document.getElementById("3")
+   // var getCategory = document.getElementById("BoxHeader")
+  //  var categoryText1 = getCategory.innerHTML
+   // var splitText = categoryText1.split('<span')[0]
+    createContent.innerHTML = stringCut
+
+    
+    
+    var check = document.getElementsByClassName("content3")
+  // for(var i = 0; i < check.length; i++){
+     //  if(!check[i].id == cutIdNumber){
+           divId.appendChild(createContent)
+    //   }
+
+  // }
+   
+
+    
 
 }
 
@@ -28,10 +50,14 @@ function removeDishes(elements){
 function createDishes(totalDish){
     for(var i = 0; i < totalDish; i++){
         var createContent = document.createElement("div")
+    createContent.setAttribute("id", "dBox" +1)
     createContent.setAttribute("class", "contentBox2")
-    createContent.innerHTML = "nya"
-    var divId = document.getElementById("2")
-    divId.appendChild(createContent)
+    createContent.setAttribute("onclick", "addToCashier(this)")
+    createContent.innerHTML = "<span class='itemCountS'>0</span> <span class='item'>Marabou</span> <span class='price'>€12.50</span>"
+
+    var divId2 = document.getElementById("2")
+    
+    divId2.appendChild(createContent)
     }
 }
 
@@ -43,22 +69,26 @@ function addToCashier(idBox){
     var idElementItem = document.getElementById(id).getElementsByClassName("item")[0]
     var idElementPrice= document.getElementById(id).getElementsByClassName("price")[0]
     
-    var item = [];
-    item.push(idElementItem)
+    var item = idElementItem.innerHTML
+    
     
     var price = idElementPrice.innerHTML
 
-    var getCategory = document.getElementById("true")
-    var categoryText = getCategory.innerHTML
 
     
-    var createContent = document.createElement("div")
+    
+    var getCategory = document.getElementById("BoxHeader")
+    var categoryText1 = getCategory.innerHTML
+    var splitText = categoryText1.split('<span')[0]
 
-    createContent.setAttribute("class", "content3text1")
-    createContent.innerHTML = categoryText + allItem(item) + price
+    
 
+    createContent.innerHTML = splitText
+    
+    createContent.innerHTML = createContent.innerHTML + item[0] + price
     var divId = document.getElementById("3")
-    divId.appendChild(createContent)
+divId.appendChild(createContent)
+    
 
 }
 
