66da031293978f6b978d841f3b4417a9faf46c0e
Sh1re-a <83682762+Sh1re-a@users.noreply.github.com>
2022-02-22 18:54:19 +0100

    Testar funktionerna med JSON Data

--- src/main/resources/templates/V2.0/index1.js
+++ src/main/resources/templates/V2.0/index1.js
@@ -1,5 +1,6 @@
 var id1;
 
+
 function showDishes(idElement){
     id1 = idElement.id
     var allId = []
@@ -9,6 +10,7 @@ function showDishes(idElement){
     var stringCut = categoryText.split('<span')[0]
     document.getElementById("BoxHeader").innerHTML = stringCut +"<div id ='hej'class='content2Text1'><span class='headtext1'>Popular</span>&nbsp; &nbsp;&nbsp;<span class='headtext2'>A/z</span>&nbsp; &nbsp;&nbsp;<span class='headtext3'>Price</span></div>"
     var cutIdNumber = id1
+    let lowerCase = stringCut.toLowerCase();
     
 
     var element = document.getElementsByClassName("contentBox2")
@@ -16,7 +18,15 @@ function showDishes(idElement){
 
     var totalDish = 14;
 
-    createDishes(totalDish)
+    var xhr = new XMLHttpRequest()
+    xhr.open("GET", "data/"+lowerCase+".json")
+    xhr.onload = function(){
+        var data = JSON.parse(this.response)
+        createDishes(data)
+    }
+    xhr.send()
+
+    
 
     var createContent = document.createElement("div")
     createContent.setAttribute("class", cutIdNumber)
@@ -68,13 +78,13 @@ function removeDishes(elements){
 
 }
 
-function createDishes(totalDish){
-    for(var i = 0; i < totalDish; i++){
-        var createContent = document.createElement("div")
-    createContent.setAttribute("id", "dBox" +1)
+function createDishes(data){
+    for(var i = 0; i < data.length; i++){
+    var createContent = document.createElement("div")
+    createContent.setAttribute("id", "dBox"+ i)
     createContent.setAttribute("class", "contentBox2")
     createContent.setAttribute("onclick", "addToCashier(this)")
-    createContent.innerHTML = "<span class='itemCountS'>0</span> <span class='item'>Marabou</span> <span class='price'>€12.50</span>"
+    createContent.innerHTML = "<span id='tja"+i+"' class='itemCountS'>"+antal +"</span> <span id='tjo"+i +"'class='item'>"+data[i].dishName +"</span> <span <span id='tjena"+i +"' class='price'>€"+data[i].price+ "</span>"
     
     var divId2 = document.getElementById("2")
     
@@ -87,12 +97,14 @@ function createDishes(totalDish){
     }
 }
 
-var antal = 0;
+var antal = 0
 
 function addToCashier(idBox){
     //TODO: Skapa en loop som lägger till
 
     var id = idBox.id
+
+
     var idElementItem = document.getElementById(id).getElementsByClassName("item")[0]
     var idElementPrice= document.getElementById(id).getElementsByClassName("price")[0]
     
@@ -119,34 +131,52 @@ function addToCashier(idBox){
             }
             }
 
-            var howMany = document.getElementById("2").children
-            
-            
+        
 
            var findElement = document.getElementsByClassName(save)[0]
 
-            //var findElement2 = document.getElementById("2").children
-
-            // for(var i = 0; i < findElement2.length; i++){ }
-
             
+
+           var theCut = id.substring(4);
+
 
             
             if(findElement.innerHTML.includes(item)){
+                
                 antal++;
                 var int  = price.substring(1);
                 var number = parseFloat(int)
                 var totalSumma = number * antal
+               document.getElementById(id).innerHTML = "<span id='tja"+theCut+"' class='itemCountS'>"+antal +"</span> <span class='item'>Marabou</span> <span class='price'>€12.50</span>"
+                
                 findElement.innerHTML = '<div class ="content31"><span class ="cashierCount"> x' + antal + '</span><span class ="vara">'+ item + '</span>' + '<span class ="priset">€' +totalSumma + '</span></div>'
             }
-            else
-           
-           findElement.innerHTML = findElement.innerHTML + '<div class ="content31"><span class ="vara">'+ item + '</span>' + '<span class ="priset">' +price + '</span></div>'
+            else {
+                
+                var findItem = document.getElementById("tjo"+theCut)
+                var findItem2 = findItem.innerHTML;
+                var findPrice = document.getElementById("tjena"+theCut)
+                var findPrice2 = findPrice.innerHTML;
+                
+            document.getElementById(id).innerHTML = "<span id='tja"+theCut+"' class='itemCountS'>"+1 +"</span> <span class='item'>"+findItem2+"</span> <span class='price'>"+findPrice2+"</span>"
+           findElement.innerHTML = findElement.innerHTML + '<div class ="content31"><span class ="cashierCount"> x' + 1 + '</span><span class ="vara">'+ item + '</span>' + '<span class ="priset">' +price + '</span></div>'
 
-    
+            }
  
     
 
+}
+
+function howManyDishesAdded(antal){
+    for (var i = 0; i < antal.length; i++){
+
+    }
+}
+
+function arraymove(arr, fromIndex, toIndex) {
+    var element = arr[fromIndex];
+    arr.splice(fromIndex, 1);
+    arr.splice(toIndex, 0, element);
 }
 
 
