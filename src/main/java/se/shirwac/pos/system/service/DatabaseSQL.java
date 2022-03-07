6c1685a3594693799636a9459cae5eecd400a70d
Sh1re-a <83682762+Sh1re-a@users.noreply.github.com>
2022-03-07 05:30:23 +0100

    tog bort ClearDB helt värdelös

--- src/main/java/se/shirwac/pos/system/service/DatabaseSQL.java
+++ src/main/java/se/shirwac/pos/system/service/DatabaseSQL.java
@@ -16,7 +16,7 @@ import java.sql.SQLException;
 //Skapat en färdig database man kan leka med, generarat datan från mocakaroGenerator
 @Service
 public class DatabaseSQL {
-    private final String url = "jdbc:mysql://b3f4c27078cef1:095e3132@us-cdbr-east-05.cleardb.net/heroku_d979d5442dba2a7?reconnect=true";
+    private final String url = "jdbc:mysql://localhost:3306/STI?";
     private final String user = "root";
     private final String password = "";
 
